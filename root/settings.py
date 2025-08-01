0cf1defe761b859c83dee115f37e8148d25746e9
Nodir <nodir.primkulov.77@gmail.com>
2025-08-01 11:10:34 +0500

    add staticfiles

--- root/settings.py
+++ root/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # bu yerda!
+    'whitenoise.middleware.WhiteNoiseMiddleware', # bu yerda!
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
