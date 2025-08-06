80d71f3eba0d9f2e0c3af77cec5be19c0dac9bc8
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2025-08-06 10:33:59 +0500

    add render.yaml

--- root/settings.py
+++ root/settings.py
@@ -82,7 +82,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static
 STATIC_URL = '/static/'
--- root/urls.py
+++ root/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
